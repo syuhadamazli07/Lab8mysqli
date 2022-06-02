f9444fa03141b90de5e6d811c229953aebf11639
syuhadamazli07 <syuhadamazli07@gmail.com>
2022-06-02 22:19:12 +0800

    commit

---
+++ con_db.php
@@ -0,0 +1,17 @@
+<?php
+define ('db_user','root');
+define ('db_password','');
+define ('db_host', 'localhost');
+define ('db_name','registration');
+
+$conn = mysqli_connect (db_host, db_user, db_password, db_name);
+$result = mysqli_query ($conn, "select * from user");
+while($row = mysqli_fetch_array($result)) {
+    foreach($row as $key=>$value) {
+        each "$key = $value <br>\n";
+    }
+}
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
+
