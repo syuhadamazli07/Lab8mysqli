6490c71eb750fde49064ecfa9dfc7f788644b88a
syuhadamazli07 <syuhadamazli07@gmail.com>
2022-06-02 22:40:46 +0800

    commit

---
+++ get_info.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form action="add_user.php" method = "post">
+            Firstname: <input type="text" name="firstname" required><br>
+            Lastname: <input type="text" name="lastname" required><br>
+            E-mail: <input type="text" name="email" required><br>
+            Password: <input type="text" name="password" required><br>
+            Registration date: <input type="date" name="reg_date" required><br>
+            <input type="submit" value="Submit Data">
+</form>
+</body>
+</html>
